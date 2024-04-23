8aec1728d4847a1dbf545c9405df927c1c6639ea
Daniel <danielghost@live.com>
2024-04-23 17:46:45 +0800

    fix: big

--- src/amhs/config.py
+++ src/amhs/config.py
@@ -5,8 +5,9 @@ configpath = "./config.yaml"
 map = './data/json/amhs/OHTC_MAP_202404191001.json'
 position = './data/json/amhs/OHTC_POSITION_202404191001.json'
 table = './data/json/amhs/TRANSFER_TABLE_202404191002.json'
+car = './data/json/amhs/TRANSFER_TABLE_202404191002.json'
 
-pathArray = [map,position,table]
+pathArray = [map,position,table,car]
 
 # log config
 logPath = "./log/sql.log"
