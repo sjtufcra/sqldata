eacce875f25958458e0c9caedfc13cbef60a0c66
Daniel <danielghost@live.com>
2024-04-24 09:20:07 +0800

    fix: bug

--- src/amhs/config.py
+++ src/amhs/config.py
@@ -18,17 +18,17 @@
 
 
 # local
-configpath = "./sqldata/config.yaml"
+configpath = "./config.yaml"
 
 # datapath
-map = './sqldata/data/json/amhs/OHTC_MAP_202404191001.json'
-position = './sqldata/data/json/amhs/OHTC_POSITION_202404191001.json'
-table = './sqldata/data/json/amhs/TRANSFER_TABLE_202404191002.json'
-car = './sqldata/data/json/amhs/vehicles_status.json'
+map = './data/json/amhs/OHTC_MAP_202404191001.json'
+position = './data/json/amhs/OHTC_POSITION_202404191001.json'
+table = './data/json/amhs/TRANSFER_TABLE_202404191002.json'
+car = './data/json/amhs/vehicles_status.json'
 
 pathArray = [map,position,table,car]
 
 # log config
-logPath = "./sqldata/log/sql.log"
+logPath = "./log/sql.log"
 logDay = 1
 logLevel = "INFO"
